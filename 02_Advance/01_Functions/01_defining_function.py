cc227bd98baf5a10944c05e4715d4850af184168
Utkershhh <yourgithubemail@gmail.com>
2026-01-11 22:39:00 +0530

    Initial commit

---
+++ 02_Advance/01_Functions/01_defining_function.py
@@ -0,0 +1,11 @@
+#defining a function-function is defined jab coder ko code ko reuse krna hota hai bina code ko repeat kre
+
+def func_to_determine_avg():
+    a = int(input("Enter the first number: "))
+    b = int(input("Enter the second number: "))
+    c = int(input("Enter the third number: "))
+
+    avg = (a+b+c)/3
+    print(avg)
+
+func_to_determine_avg()
---
+++ 02_Advance/01_Functions/02_functions_with_arguements.py
@@ -0,0 +1,7 @@
+def greet(name, dhanywaad):
+    print("Good Morning,",name)
+    print(dhanywaad)
+
+greet("Rahul", "Thank You❤️")
+greet("Priya", "Thank You❤️")
+greet("Vijay", "Thank You❤️")
