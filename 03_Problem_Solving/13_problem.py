cea0085c711efb31bf9018a558e4c74abbd87705
utk.dev <utkershpandyaa@gmail.com>
2026-01-13 23:37:05 +0530

    Update 13_problem.py

--- 03_Problem_Solving/13_problem.py
+++ 03_Problem_Solving/13_problem.py
@@ -1,4 +1,4 @@
 n = int(input("Enter the number: "))
 for i in range(1,n+1):
-    print(" "* (2*i-1), end="")
-    print("*" * (n-i), end="")
+    print(" "* (n-i), end="")
+    print("*" * (2*i-1))
